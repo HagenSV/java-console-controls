bb90290d230f1c8bdd376f490e0f54dcab2b3c52
HAGENSV22 <hagensv22@gcc.edu>
2025-05-02 18:46:08 -0400

    Console Starter

---
+++ src/main/java/com/hagen/Console.java
@@ -0,0 +1,134 @@
+package com.hagen;
+
+public class Console {
+
+    public enum Mode {
+        AFTER(0),
+        BEFORE(1),
+        ALL(2);
+
+        private final int value;
+
+        Mode(int value){
+            this.value = value;
+        }
+    }
+
+    /**
+     * Move the cursor up 1 line.
+     */
+    public static void cursorUp() { cursorUp(1); }
+
+    /**
+     * Move the cursor up n lines.
+     * @param n the number of lines to move up
+     */
+    public static void cursorUp(int n) {
+        System.out.printf("\033[%dA", n);
+    }
+
+    /**
+     * Move the cursor down 1 line.
+     */
+    public static void cursorDown() { cursorDown(1); }
+
+    /**
+     * Move the cursor down n lines.
+     * @param n the number of lines to move down
+     */
+    public static void cursorDown(int n) {
+        System.out.printf("\033[%dB", n);
+    }
+
+    /**
+     * Move the cursor forward 1 character.
+     */
+    public static void cursorForward() { cursorForward(1); }
+
+    /**
+     * Move the cursor forward n characters.
+     * @param n the number of characters to move forward
+     */
+    public static void cursorForward(int n) {
+        System.out.printf("\033[%dC", n);
+    }
+
+    /**
+     * Move the cursor backward 1 character.
+     */
+    public static void cursorBackward() { cursorBackward(1); }
+
+    /**
+     * Move the cursor backward n characters.
+     * @param n the number of characters to move backward
+     */
+    public static void cursorBackward(int n) {
+        System.out.printf("\033[%dD", n);
+    }
+
+    /**
+     * Clear the screen and move the cursor to the top left corner.
+     */
+    public static void clear() {
+        setCursorPos(1,1);
+        System.out.print("\033[2J\033[3J");
+        System.out.flush();
+    }
+
+    public static void clear(Mode mode){
+        System.out.printf("\033[%dJ",mode.value);
+        System.out.flush();
+    }
+
+    public static void clearLine(){
+        clearLine(Mode.ALL);
+    }
+
+    public static void clearLine(Mode mode){
+        System.out.printf("\033[%dK",mode.value);
+        System.out.flush();
+    }
+
+    /**
+     * Clear the screen and move the cursor to the top left corner.
+     * @param x the x coordinate of the cursor
+     * @param y the y coordinate of the cursor
+     */
+    public static void setCursorPos(int x, int y){
+        System.out.printf("\033[%d;%dH", y, x);
+    }
+
+    /**
+     * Set the text color to the specified RGB value.
+     * @param r the red component (0-255)
+     * @param g the green component (0-255)
+     * @param b the blue component (0-255)
+     */
+    public static void setTextColor(int r, int g, int b) {
+        System.out.printf("\033[38;2;%d;%d;%dm",r,g,b);
+    }
+
+    /**
+     * Set the background color to the specified RGB value.
+     * @param r the red component (0-255)
+     * @param g the green component (0-255)
+     * @param b the blue component (0-255)
+     */
+    public static void setBackgroundColor(int r, int g, int b) {
+        System.out.printf("\033[48;2;%d;%d;%dm",r,g,b);
+    }
+
+    /**
+     * Clear the text color to the default color.
+     */
+    public static void clearTextColor() {
+        System.out.print("\033[39m");
+    }
+
+    /**
+     * Clear the background color to the default color.
+     */
+    public static void clearBackgroundColor(){
+        System.out.print("\033[49m");
+    }
+}
